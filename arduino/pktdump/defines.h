d20b5bf00084164923b8cb1edcaf0221d43ccf85
Mika <mika.kousa@gmail.com>
2012-10-29 02:23:04 +0200

    Defined structs for Ethernet, ARP and ICMP headers. Added many useful #defines for protocols. Prints now ARP and ICMP headers. Function to parse IP protocols and its payload (currently only initial implementation for ICMP). More error checks for IP header. Improved code commenting. Removed function l3_proto_str in favor of the new style ether_protocol_handlers[i].printer().

--- arduino/pktdump/defines.h
+++ arduino/pktdump/defines.h
@@ -1,11 +1,38 @@
 // linux/if_ether.h
 #define ETH_FRAME_LEN 1514
+#define ETH_ALEN 6
 
 // net/ethernet.h
 #define ETHERTYPE_IP 0x0800
 #define ETHERTYPE_ARP 0x0806
 #define ETHERTYPE_VLAN 0x8100
 #define ETHERTYPE_IPV6 0x86DD
+
+// net/ethernet.h
+struct ether_header {
+  uint8_t  ether_dhost[ETH_ALEN]; 
+  uint8_t  ether_shost[ETH_ALEN];
+  uint16_t ether_type;
+} __attribute__ ((__packed__));
+
+// net/if_arp.h
+struct arphdr {
+    uint16_t ar_hrd;
+    uint16_t ar_pro;
+    uint8_t ar_hln;
+    uint8_t ar_pln;
+    uint16_t ar_op;
+    byte sha[0];
+};
+#define ARPOP_REQUEST 1
+#define ARPOP_REPLY 2
+
+// netinet/in.h
+enum {
+  IPPROTO_ICMP = 1,
+  IPPROTO_TCP = 6,
+  IPPROTO_UDP = 17,
+};
 
 // netinet/ip.h
 struct iphdr {
@@ -24,17 +51,38 @@ struct iphdr {
   /* The options start here. */
 };
 
+struct icmphdr {
+  uint8_t type;
+  uint8_t code;
+  uint16_t checksum;
+  union {
+    struct {
+      uint16_t id;
+      uint16_t sequence;
+    } echo;
+    uint32_t gateway;
+    struct {
+      uint16_t __unused;
+      uint16_t mtu;
+    } frag;
+  } un;
+};
+
+#define ICMP_ECHOREPLY 0
+#define ICMP_ECHO 8
+
 // type of the function that can handle L3 layer packets
 typedef String (*l3_printer)(byte *l3, unsigned int payload_len);
-                                 
+
 extern String ip_print(byte *l3, unsigned int payload_len);
+extern String arp_print(byte *l3, unsigned int payload_len);
 
 static struct {
   uint16_t ether_proto;
   l3_printer printer;
 } ether_protocol_handlers[] = {
  { ETHERTYPE_IP, ip_print },
- { ETHERTYPE_ARP, NULL },
+ { ETHERTYPE_ARP, arp_print },
  { ETHERTYPE_VLAN, NULL },
  { ETHERTYPE_IPV6, NULL },
  { 0, NULL }
--- arduino/pktdump/pktdump.ino
+++ arduino/pktdump/pktdump.ino
@@ -2,13 +2,23 @@
  * Mini tcpdump style application for Arduino and ENC28J60 Ethernet controller.
  * Uses the EtherCard library. This application shows information on received
  * Ethernet frames on Serial and/or LCD.
+ *
+ * Currently implemented (more or less):
+ * -Ethernet header
+ *  -ARP header
+ *  -IPv4 header
+ *   -ICMP header
+ *    -ECHO reply/request
  */
 
 // TODO:
-// more length checks, more handlers for L3/L4 protocols, code commenting and cleanup,
-// fix LCD code for other than 16x2 displays
+// more length validation checks
+// more handlers for L3/L4 protocols
+// code commenting and cleanup
+// generalize LCD code for other than 16x2 displays
+// IP: check DSCP/ECN flags
 
-#define prg_version "v0.5"
+#define prg_version "v0.6"
 
 #include <EtherCard.h>
 #include "defines.h"
@@ -17,7 +27,7 @@
 #define HAVE_LCD
 #ifdef HAVE_LCD
  #include <LiquidCrystal.h>
- LiquidCrystal lcd(8, 13, 9, 4, 5, 6, 7);
+ LiquidCrystal lcd(8, 13, 9, 4, 5, 6, 7); // this works for me, change for your case
 
  #define LCD_COLS 16
  #define LCD_ROWS 2
@@ -31,7 +41,8 @@ static byte myip[] = { 192,168,20,123 }; // not needed ?
 //#define CAPTURE_SIZE (14+20+1) // to test truncated frames
 byte Ethernet::buffer[CAPTURE_SIZE];
 
-void printstr(String s, int print_serial, int print_lcd = 0) {
+// print string s on Serial if print_serial is true, print s also on LCD if print_lcd is true
+void printstr(String s, boolean print_serial, boolean print_lcd = 0) {
 #ifdef HAVE_LCD
   if (print_lcd) {
     lcd.clear();
@@ -46,18 +57,91 @@ void printstr(String s, int print_serial, int print_lcd = 0) {
     Serial.println(s);
 }
 
-// parse IP packets
+// parse ARP packets
+String arp_print(byte *l3, unsigned int payload_len) {
+  String s;
+  struct arphdr *arp = (struct arphdr *) l3;
+  uint16_t ar_hrd = ntohs(arp->ar_hrd);
+  uint16_t ar_pro = ntohs(arp->ar_pro);
+  uint8_t ar_hln = arp->ar_hln;
+  uint8_t ar_pln = arp->ar_pln;
+  uint16_t ar_op = ntohs(arp->ar_op);
+  int i;
+  
+  s += String("ARP ");
+  s += "htype=" + String(ar_hrd);
+  s += " ptype=0x" + String(ar_pro, HEX);
+  s += " hlen=" + String(ar_hln);
+  s += " plen=" + String(ar_pln);
+  s += " oper=" + String(ar_op) + "(" + (ar_op == ARPOP_REQUEST ? "request" : ar_op == ARPOP_REPLY ? "reply" : "unknown operation") + ")";
+
+  byte *p = arp->sha;
+  s += " sender HW=0x";
+  for (i = 0; i < ar_hln; i++, p++) {
+    if (*p <= 0xf) s += '0';
+    s += String(*p, HEX);
+  }
+
+  s += " sender protoaddr=0x";
+  for (i = 0; i < ar_pln; i++, p++) {
+    if (*p <= 0xf) s += '0';
+    s += String(*p, HEX);
+  }
+
+  if (ar_op != ARPOP_REQUEST) {
+    s += " target HW=0x";
+    for (i = 0; i < ar_hln; i++, p++) {
+      if (*p <= 0xf) s += '0';
+      s += String(*p, HEX);
+    }
+  } else {
+    p += ar_hln;
+  }
+  
+  s += " target protoaddr=0x";
+  for (i = 0; i < ar_pln; i++, p++) {
+    if (*p <= 0xf) s += '0';
+    s += String(*p, HEX);
+  }
+
+  return s;
+}
+
+// print the transport protocol in the IP packet payload
+String ip_protocol_print(byte *l4, unsigned int payload_len, uint8_t protocol) {
+  String s;
+
+  switch (protocol) {
+      case IPPROTO_ICMP: {
+        struct icmphdr *icmp = (struct icmphdr *) l4;
+        s += ", ICMP len=" + String(payload_len) + " type=" + String(icmp->type) + " code=" + String(icmp->code) + " checksum=" + String(icmp->checksum);
+        if (icmp->type == ICMP_ECHO || icmp->type == ICMP_ECHOREPLY)
+          s += ", echo " + String((icmp->type == ICMP_ECHO ? "request" : "reply")) +
+               " id=" + String(ntohs(icmp->un.echo.id)) + " seq=" +
+               String(ntohs(icmp->un.echo.sequence)) + " datalen=" +
+               String(payload_len-sizeof(struct icmphdr));
+        break;
+      }
+      default: {
+        s += ", L4 len=" + String(payload_len);
+        break;
+      }
+  }
+  return s;  
+}
+
+// parse IPv4 packets
 String ip_print(byte *l3, unsigned int payload_len) {
   String s;
   struct iphdr *ip = (struct iphdr *) l3;
 
-  s += String("IP ");// + String(payload_len);
+  s += String("IP ");
 
   //char str[200];
   //sprintf(str, "<len=%u>",ip->tot_len); 
   //s += str;
   if ((payload_len < sizeof(struct iphdr)) || (payload_len < (ip->ihl << 2)) ) {
-    s += "[truncated]";
+    s += "[truncated IP]";
     return s;
   }
   s += "ver=" + String(ip->version);
@@ -78,191 +162,81 @@ String ip_print(byte *l3, unsigned int payload_len) {
   s += " daddr=";
   s += ipv4str;
 
-if (payload_len < ntohs(ip->tot_len)) {
- // todo truncated L4 ? 
-}
+  // check for truncated L4 payload
+  if (payload_len < ntohs(ip->tot_len)) {
+    s += ", [truncated by " + String((ntohs(ip->tot_len) - payload_len - (ip->ihl << 2))) + " bytes";
+    return s;
+  }
 
-  payload_len -= ip->ihl << 2;
+  // controller returns always minimum of 60 byte frames (see sections 5.1, 5.1.6, and
+  // ENC28J60::packetReceive seems to remove the four octet CRC) so after checks
+  // we set payload length to be the total length field of the IP header decreased
+  // by the header length
+  payload_len = ntohs(ip->tot_len) - (ip->ihl << 2);
+//  payload_len -= ip->ihl << 2;
+  //s += ", L4 len=" + String(payload_len);
   if (payload_len > 0) {
-    // parse L4 
-    s += ", L4 len=" + String(payload_len);
+    // if there are any L4 payload left, parse it
+    s += ip_protocol_print((byte *)ip + (ip->ihl << 2), payload_len, ip->protocol);
   }
   return s;
-}
-
-
-// TODO rename
-// get Layer 2 (link) information
-static const String l2_proto_str(byte *l2, unsigned int payload_len) {
-  String s;
-  int i;
-  byte *p;
-  uint16_t ethproto;
- 
-  // src+dst MAC
-  p = ether.buffer + ETH_SRC_MAC;
-  for (i = 0; i < 6; i++) {
-    if (*(p + i) <= 0xf) s += '0';
-    s += String(*(p + i), HEX);
-    if (i != 5) s += ':';
-  }
-  p = ether.buffer + ETH_DST_MAC;
-  s += '>';
-  for (i = 0; i < 6; i++) {
-    if (*(p + i) <= 0xf) s += '0';
-    s += String(*(p + i), HEX);
-    if (i != 5) s += ':';
-  }
-
-  s += " len=" + String(payload_len) + ", ";
- 
- ethproto = (*(l2 + ETH_TYPE_H_P) << 8) + *(l2 + ETH_TYPE_L_P);  
-// for (i = 0; ether_protocol_handlers[i].str; i++) {
- for (i = 0; ether_protocol_handlers[i].ether_proto; i++) {
-  if (ether_protocol_handlers[i].ether_proto == ethproto) {
-   if (ether_protocol_handlers[i].printer) {
-     // call L3 level protocol handler if it is defined
-     s += ether_protocol_handlers[i].printer(l2 + ETH_HEADER_LEN, payload_len - ETH_HEADER_LEN);
-   return s;
-
-   } else {
-     // protocol known but no L3 level protocol handler implemented
-//     s += "protocol 0x" + String(ethproto, HEX) + " len=" + String(payload_len - ETH_HEADER_LEN);
-   }
-//   return s;
-//    break;
-  }
- }
- 
-// printstr("imp " + String(ethproto, HEX), 1, 1);
-// if (ether_protocol_handlers[i].str) {
-   //s += String(ether_protocol_handlers[i].str) + ": ";
-//   if (ether_protocol_handlers[i].printer)
-//     s += ether_protocol_handlers[i].printer(l2 + ETH_HEADER_LEN, payload_len - ETH_HEADER_LEN);
-//   else
- //    s += "len=" + String(payload_len);
- 
-//} else {
-  // unknown protocol, print the protocol number and its payload length
-   s += "protocol 0x" + String(ethproto, HEX) + " len=" + String(payload_len - ETH_HEADER_LEN);
-// }
- 
-  return s;
-}
-
-// get Layer 3 (network) information
-static const String l3_proto_str(byte *l3, unsigned int payload_len, uint16_t l3_proto,
-                                 int *l3_len, uint16_t *l4_proto) {
-  String s;
-  *l3_len = 0;
-  *l4_proto = 0; // todo: check if 0 is valid proto for the L3 protocol
-  printstr("payload_len l3="+String(payload_len), 1);
-
-  switch (l3_proto) {
-    case ETHERTYPE_IP: {
-      struct iphdr *ip = (struct iphdr *) l3;
-//char str[200];
-//sprintf(str, "<len=%u>",ip->tot_len); 
-//s += str;
-      if ((payload_len < sizeof(struct iphdr)) || (payload_len < (ip->ihl << 2)) ) {
-        *l3_len = -1;
-        return s;
-      }
-      *l3_len = ip->ihl << 2;
-      *l4_proto = ip->protocol;
-      s += "ver=" + String(ip->version);
-      s += " hl=" + String(ip->ihl << 2);
-      s += " TOS=" + String(ip->tos); // MASK ?, TODO DSCP/ECN
-      s += " totlen=" + String(ntohs(ip->tot_len));
-      s += " ID=" + String(ntohs(ip->id));
-      s += " flags=" + String(ntohs(ip->frag_off) >> 13, BIN) + 'b';
-      s += " fragoff=" + String(ntohs(ip->frag_off) & 0x1FFF);
-      s += " ttl=" + String(ip->ttl);
-      s += " protocol=" + String(ip->protocol);
-      s += " csum=0x" + String(ntohs(ip->check), HEX);
-      char ipv4str[16];
-      ether.makeNetStr(ipv4str, (byte *)&(ip->saddr), 4, '.', 10);
-      s += " saddr=";
-      s += ipv4str;
-      ether.makeNetStr(ipv4str, (byte *)&(ip->daddr), 4, '.', 10);
-      s += " daddr=";
-      s += ipv4str;
-      break;
-    }
-    default: {
-      s += "protocol 0x" + String(l3_proto, HEX);
-      s += " len=" + String(payload_len);
-      *l3_len = payload_len; // for unknown L3 protocols so that L4 is not anymore done
-      break;
-    }
-  }
-  return s;
-}
-
-// get Layer 4 (transport) information
-static const String l4_proto_str(byte *l4, unsigned int payload_len, uint16_t l4_proto) {
-  String s;
-  
-  printstr("payload_len l4="+String(payload_len), 1);
-  s += "len=" + String(payload_len);
-  // TODO
-  return s;
-}
-
-int led = HIGH;
-
-void switch_led(void) {
-  // switch LED state when a frame was received
-  digitalWrite(LED_PIN, led);
-  led = led == HIGH ? LOW : HIGH;
 }
 
 unsigned long int recvd_frames = 0;
 
-String dump_frame(byte *frame, word framelen) {
+// dump Ethernet header and parse higher layers
+String dump_frame(byte *frame, word frame_len) {
   String s;
-  String l2_str, l3_str, l4_str;
-  uint16_t l3_proto = 0, l4_proto = 0;
-  unsigned int payload_len = framelen;
+  struct ether_header *eth = (struct ether_header *) frame;
+  int i;
+  byte *p;
 
-  s = '#' + String(recvd_frames);
+  s = '#' + String(recvd_frames) + ' ';
 
-  if (framelen < ETH_HEADER_LEN) {
-     s += String(" short frame, frame length=") + framelen;
+  if (frame_len < ETH_HEADER_LEN) {
+     s += String("short frame, frame length=") + frame_len;
      return s;
   }
 
-  //l2_str 
-  s += ' ' + l2_proto_str(ether.buffer, framelen);
-//  s += " L2:" + l2_str;
-#if 0
-  printstr("payload_len l2="+String(payload_len), 1);
+  p = eth->ether_shost;
+  for (i = 0; i < ETH_ALEN; i++) {
+    if (*(p + i) <= 0xf) s += '0';
+    s += String(*(p + i), HEX);
+    if (i != ETH_ALEN-1) s += ':';
+  }
+  s += '>';
+  p = eth->ether_dhost;
+  for (i = 0; i < ETH_ALEN; i++) {
+    if (*(p + i) <= 0xf) s += '0';
+    s += String(*(p + i), HEX);
+    if (i != ETH_ALEN-1) s += ':';
+  }
 
-  l3_proto = (*(ether.buffer + ETH_TYPE_H_P) << 8) + *(ether.buffer + ETH_TYPE_L_P);  
-  int l3_len = 0;
-  
-  payload_len -= ETH_HEADER_LEN; // remaining length in buffer now, L3+L4..
-  
-  l3_str = l3_proto_str(ether.buffer + ETH_HEADER_LEN, payload_len, l3_proto, &l3_len, &l4_proto);
-  //if (l3_str.length() > 0) {
-  if (l3_len >= 0) {
-    s += "\n L3:" + l3_str;
-    //s = l3_str;
+  s += " len=" + String(frame_len) + ", ";
 
-    payload_len -= l3_len; // remaining length in buffer, L4..
-    if (payload_len > 0) { // check L4 only if we knew how to handle L3 and there is L4 data left
-      l4_str = l4_proto_str(ether.buffer + ETH_HEADER_LEN + l3_len, payload_len, l4_proto);
-      if (l4_str.length() > 0) {
-        s += "\n  L4:" + l4_str;
-      } else {
-        //s += " unknown L4";
+  uint16_t ethproto = ntohs(eth->ether_type);
+
+  for (i = 0; ether_protocol_handlers[i].ether_proto; i++) {
+    if (ether_protocol_handlers[i].ether_proto == ethproto) {
+      if (ether_protocol_handlers[i].printer) {
+        // call L3 level protocol handler if it is defined
+        s += ether_protocol_handlers[i].printer(frame + ETH_HEADER_LEN, frame_len - ETH_HEADER_LEN);
+        return s;
       }
     }
-  } else if (l3_len < 0) {
-    s += " (truncated L3)";
   }
-#endif
+ 
+  // unknown protocol, print the protocol number and its payload length
+  s += "protocol 0x" + String(ethproto, HEX) + " len=" + String(frame_len - ETH_HEADER_LEN);
+
   return s;
+}
+
+void switch_led(void) {
+  static int led = HIGH;
+  // switch LED state when a frame was received
+  digitalWrite(LED_PIN, led);
+  led = led == HIGH ? LOW : HIGH;
 }
 
 void setup () {
@@ -274,17 +248,16 @@ void setup () {
   while (true) {
     uint8_t rev = ether.begin(CAPTURE_SIZE, mymac);
     if (rev == 0) {
-      printstr("Failed to access Ethernet controller", 1, 1);
+      printstr("Failed to access Ethernet controller", true, true);
       delay(1000);
     } else {
       printstr("pktdump " prg_version ", controller revision=" + String(rev) +
-               ", capture size=" + String(CAPTURE_SIZE), 1, 1);
+               ", capture size=" + String(CAPTURE_SIZE), true, true);
       break;      
     }
   }
-  //ether.staticSetup(myip); // not needed ?
+  //ether.staticSetup(myip); // actually not needed in this app
   ether.enableBroadcast();
-  
   //ether.xx(); // promiscuous mode
 }
 
@@ -296,7 +269,7 @@ void loop () {
     recvd_frames++;
     switch_led();
     String s = dump_frame(ether.buffer, framelen);
-    printstr(s, 1, 1);
+    printstr(s, true, true);
   }
 }
 
